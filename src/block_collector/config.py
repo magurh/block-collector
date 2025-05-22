78e2cff0f591a8c21b9c5c299af8a6de5b51aa2c
magurh <magureanuhoria@gmail.com>
2025-05-22 16:05:39 +0100

    fix: add .env file

--- src/block_collector/config.py
+++ src/block_collector/config.py
@@ -1,5 +1,15 @@
+import os
 from dataclasses import dataclass
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def load_env_var(var_name: str) -> str:
+    """Loads and validates environment variables."""
+    return os.getenv(var_name, default="")
 
 
 @dataclass
@@ -11,17 +21,23 @@ class NetworkConfig:
     wait_time: int
 
 
+flr_explorer_url = load_env_var("FLARE_EXPLORER_URL")
+
 flr_config = NetworkConfig(
     network="flare",
-    explorer_url="https://flare-explorer.flare.network/api/v2/blocks?type=block",
+    explorer_url=flr_explorer_url,
     wait_time=70,
 )
 
+sgb_explorer_url = load_env_var("SONGBIRD_EXPLORER_URL")
+
 sgb_config = NetworkConfig(
     network="songbird",
-    explorer_url="https://songbird-explorer.flare.network/api/v2/blocks?type=block",
+    explorer_url=sgb_explorer_url,
     wait_time=40,
 )
+
+gcp_bucket = load_env_var("GCP_BUCKET_NAME")
 
 
 @dataclass
@@ -34,7 +50,7 @@ class Config:
 
 
 config = Config(
-    gcp_bucket="block-collector",
+    gcp_bucket=gcp_bucket,
     upload_interval=100,
     output_dir=Path("data"),
 )
